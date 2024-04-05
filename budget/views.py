3e89a0c584f8bfa0f144143df8baecf9b88979af
Christian Go/CAN <christian.go@ca.cushwake.com>
2024-04-05 09:08:22 -0600

    Fixed authentication bug

--- budget/views.py
+++ budget/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from .serializers import (CategorySerializer, LedgerItemSerializer, 
     BudgetItemSerializer)
 from .models import Category, LedgerItem, BudgetItem
@@ -13,22 +14,28 @@ class CategoryView(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
 
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
+
 class LedgerItemView(viewsets.ModelViewSet):
     serializer_class = LedgerItemSerializer
     queryset = LedgerItem.objects.all()
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
 
 class BudgetItemView(viewsets.ModelViewSet):
     serializer_class = BudgetItemSerializer
     queryset = BudgetItem.objects.all()
 
     def create(self, request):
-        print(request.data)
         category = request.data.get('category')
         category_type = request.data.get('type')
         amount = request.data.get('amount')
-        owner = request.data.get('owner')
-        
-        user = User.objects.get(id=owner)
+        user = User.objects.get(id=request.user.id)
     
         new_category = Category(owner=user, name=category, type=category_type)
         new_category.save()
@@ -40,15 +47,18 @@ class BudgetItemView(viewsets.ModelViewSet):
         return Response(status=200)
 
 @api_view(('GET',))
-def ledger_view(self):
-    queryset = LedgerItem.objects.all().values('id', 'date', 'category__name', 
+def ledger_view(request):
+    user = User.objects.get(id=request.user.id)
+    queryset = LedgerItem.objects.filter(owner=user)
+    queryset = queryset.values('id', 'date', 'category__name', 
         'category__type', 'amount')
     return(Response(queryset))
 
 @api_view(('GET',))
-def budget_view(self):
-    budget_items = BudgetItem.objects.all()
-    ledger_items = LedgerItem.objects.all()
+def budget_view(request):
+    user = User.objects.get(id=request.user.id)
+    budget_items = BudgetItem.objects.filter(owner=user)
+    ledger_items = LedgerItem.objects.filter(owner=user)
     budget = BudgetData(budget_items, ledger_items)
     budget.compile()
     return(Response(budget.data))
