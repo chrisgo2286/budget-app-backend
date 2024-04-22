846b601ee3e451105a65dbe6cc2eaeceb1f62fcc
Christian Go <chrisgo2286@yahoo.com>
2024-04-22 17:16:36 -0400

    Added allauth.socialaccount

--- backend/settings.py
+++ backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
     'dj_rest_auth.registration',
     'django.contrib.admin',
     'django.contrib.auth',
